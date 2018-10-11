df001af2c5262f2b5343852510b06707b2cf757a
James Wescott <james@wescottdesign.com>
2018-10-11 11:31:32 -0700

    Started implementing Rotation

--- lib/reward.rb
+++ lib/reward.rb
@@ -1,3 +1,47 @@
 #!/bin/ruby
 
+class Relic
+	attr_reader :chance, :id
+	def initialize(id, chance)
+		@id = id
+		@chance = chance
+	end #def Relic.new
+	def tier() return $relics_by_id[@id][:tier] end
+	def name() return $relics_by_id[@id][:name] end
+	def fullname() return $relics_by_id[@id][:fullName] end
+end #class Relic
 
+def newRelic(jh)
+	rid = jh["_id"]; return nil unless rid
+	unless ((r = $relics_by_id[rid]))
+		r = ($relics_by_id[rid] = Hash.new)
+		arr = jh["itemName"].split
+		r[:tier] = arr[0].intern
+		r[:name] = arr[1]
+		r[:fullName] = jh["itemName"]
+		# if not $relics_by_name[r[:tier]]
+		# 	$relics_by_name[r[:tier]] = {r[:name] => {nodes: []}}
+		# elsif not $relics_by_name[r[:tier]][r[:name]]
+		# 	$relics_by_name[r[:tier]][r[:name]] = {nodes: []}
+		# end #if
+	end #unless
+	relic = $relics_by_name[r[:tier]][r[:name]]
+	relic[:id] = rid unless relic[:id]
+	chance = jh["chance"].to_f / 100.0
+	return Relic.new(rid,chance)
+end #def newRelic
+
+class OtherReward
+	attr_reader :chance, :name
+	def initialize(reward)
+		@id = reward["_id"]
+		@name = reward["itemName"]
+		@chance = reward["chance"].to_f / 100.0
+	end
+end
+
+class Rotation
+	attr_reader :num_by_tier, :chance_tier, :chance_each, :tiers
+	def initialize(pool)
+	end #Rotation.new
+end #class Rotation
---
+++ spec/rotation_spec.rb
@@ -0,0 +1,98 @@
+require 'reward'
+require 'json'
+require 'set'
+
+RSpec.describe Rotation, "" do
+	#tests
+	context "Interception T1 [A]" do
+		before(:all) do
+			@reward = Rotation.new([{
+				"_id": "c0400ac7082c2f3d811e47ca9b7a8ae8",
+				"itemName": "Vitality",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "0bd85e0ab15774ad9a7ecc9340e4e7ad",
+				"itemName": "Magazine Warp",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "3cb0fa05c766f182e58bb10462d2b3a4",
+				"itemName": "Trick Mag",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "830b0bc4dbe549d5cb907162e67d7682",
+				"itemName": "Synthula",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "49bcc45ccb51d3736b159cb10e3f5594",
+				"itemName": "Lith H2 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "9786fe2fcc5920398e9e3d8f28881d21",
+				"itemName": "Lith P1 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "5d58af8d5e702a2e34accd1c20151a9c",
+				"itemName": "Lith O1 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "5a11cc6ac5f4f5d6ef6af2323cc44baa",
+				"itemName": "Lith Z2 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "10a4f1fc42c313fe75d4ff958b85ee46",
+				"itemName": "Lith C3 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}, {
+				"_id": "a5e70251bfe3077c21ac012179e76df1",
+				"itemName": "Lith B5 Relic",
+				"rarity": "Uncommon",
+				"chance": 10
+			}]) # Interception T1 Rotation A as of 2018-10-04
+		end #before(:all)
+
+		describe '@num_by_tier' do
+			it "knows the number of rewards" do
+				expect(@reward.num_by_tier).to eql({
+					Lith: 6,
+					non: 4,
+					any: 6,
+					all: 10
+				})
+			end #it knows num of rewards
+		end #describe @num_by_tier
+
+		describe '@chance_tier' do
+			it "knows the chances of each tier" do
+				expect(@reward.chance_tier).to eql({
+					Lith: 0.6,
+					non: 0.4,
+					any: 0.6
+				})
+			end #it knows chance of tier
+		end #describe '@chance_tier'
+
+		describe '@chance_each' do
+			it "knows the avg chance of a single reward" do
+				expect(@reward.chance_each).to eql({
+					Lith: 0.1,
+					non: 0.1
+				})
+			end #it knows chance of each
+		end #describe @chance_each
+
+		describe '@tiers' do
+			it "knows which tiers of rewards it has" do
+				expect(@reward.tiers).to eql(Set[:Lith, :non])
+			end #it knows which tiers it has
+		end #describe @tiers
+	end #context 6 lith relics 4 non
+end #describe Rotation
