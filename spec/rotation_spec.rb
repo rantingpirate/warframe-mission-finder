a50ee333f8b9e9e48469adfc10067658b15e3b99
James Wescott <james@wescottdesign.com>
2018-10-12 13:50:15 -0700

    Moved test results to test data file to ease adding/changing datasets.

--- spec/rotation_spec.rb
+++ spec/rotation_spec.rb
@@ -3,91 +3,47 @@ require 'json'
 require 'set'
 require "match_fhasheq"
 
+$relics_by_id = Hash.new
+$relics_by_name = Hash.new
+
+$test_sets = JSON.parse(File.read('spec/fixtures/rotation.json'))["testsets"]
+$test_sets.each{|ts|
+	ts.transform_keys!{|k| k.intern}
+		.reject{|k,v| :data == k}
+		.transform_values!{|v| v.transform_keys!{|k| k.intern}}
+	ts[:rotation] = Rotation.new(ts[:data])
+}
+
 RSpec.describe Rotation, "" do
-	before(:all) do
-		$relics_by_id = Hash.new
-		$relics_by_name = Hash.new
-	end
-	#tests
-	context "" do
-		before(:all) do
-			json = JSON.parse(File.read('spec/fixtures/rotation.json'))
-
-			#T1 Interception A - Lith + non
-			@reward1 = Rotation.new(json["T1InterceptionA"])
-
-			#T2 Interception C - Neo
-			@reward2 = Rotation.new(json["T2InterceptionC"])
-
-			#DS Defence C - Meso + Neo + non
-			@reward3 = Rotation.new(json["DSDefenseC"]) #defenSe because 'MURICA!!!
-
-		end #before(:all)
-
-		describe '@num_by_tier' do
-			it "knows the number of rewards" do
-				expect(@reward1.num_by_tier).to eql({
-					Lith: 6,
-					non: 4,
-					relic: 6,
-					all: 10
-				})
-				expect(@reward2.num_by_tier).to eql({
-					Neo: 9,
-					relic: 9,
-					all: 9
-				})
-				expect(@reward3.num_by_tier).to eql({
-					non: 13,
-					Meso: 7,
-					Neo: 9,
-					relic: 16,
-					all: 29,
-				})
+	describe '@num_by_tier' do
+		$test_sets.each.with_index{|testset,i|
+			it "knows the number of rewards (#{i+1})" do
+				rot = testset[:rotation]
+				ans = testset[:num_by_tier]
+				expect(rot.num_by_tier).to eql(ans)
 			end #it knows num of rewards
-		end #describe @num_by_tier
+		}
+	end #describe @num_by_tier
 
-		describe '@chance_tier' do
-			it "knows the chances of each tier" do
-				expect(@reward1.chance_tier).to be_equal_hash_within(1e-7, {
-					Lith: 6 * 0.1,
-					non: 4 * 0.1,
-					relic: 0.6
-				})
-				expect(@reward2.chance_tier).to be_equal_hash_within(1e-9, {
-					Neo: 1.0,
-					relic: 1.0
-				})
-				expect(@reward3.chance_tier).to be_equal_hash_within(1e-7, {
-					Neo: 0.0158 * 9,
-					Meso: 0.0759 * 7,
-					relic: 0.0158 * 9 + 0.0759 * 7,
-					non: 0.0759 * 3 + 0.0158 * 5 + 0.004 * 5
-				})
+	describe '@chance_tier' do
+		$test_sets.each.with_index{|testset,i|
+			it "knows the chances of each tier (#{i+1})" do
+				rot = testset[:rotation]
+				ans = testset[:chance_tier]
+				expect(rot.chance_tier).to be_equal_hash_within(1e-7, ans)
 			end #it knows chance of tier
-		end #describe '@chance_tier'
+		}
+	end #describe '@chance_tier'
 
-		describe '@chance_each' do
-			it "knows the avg chance of a single reward" do
-				expect(@reward1.chance_each).to be_equal_hash_within(1e-7, {
-					Lith: 0.1,
-					non: 0.1
-				})
-				expect(@reward2.chance_each).to be_equal_hash_within(1e-7, {
-					Neo: 1.0 / 9
-				})
-				expect(@reward3.chance_each).to be_equal_hash_within(1e-7, {
-					Neo: 0.0158,
-					Meso: 0.0759,
-					non: (0.0759 * 3 + 0.0158 * 5 + 0.004 * 5)/13
-				})
+	describe '@chance_each' do
+		$test_sets.each.with_index{|testset,i|
+			it "knows the avg chance of a single reward (#{i+1})" do
+				rot = testset[:rotation]
+				ans = testset[:chance_each]
+				expect(rot.chance_each).to be_equal_hash_within(1e-7, ans)
 			end #it knows chance of each
-		end #describe @chance_each
+		}
+	end #describe @chance_each
 
-		describe '@tiers' do
-			it "knows which tiers of rewards it has" do
-				expect(@reward1.tiers).to eql(Set[:Lith, :non])
-			end #it knows which tiers it has
-		end #describe @tiers
-	end #context 6 lith relics 4 non
+	#I really should test @tiers, but I don't wanna.
 end #describe Rotation
