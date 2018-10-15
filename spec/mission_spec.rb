ea1f76df63560ea31cbaff5fdd90a75975ffddb2
James Wescott <james@wescottdesign.com>
2018-10-15 12:23:48 -0700

    Swapped tier for tier_rot

--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -10,50 +10,53 @@ require 'match_fhasheq'
 RSpec.shared_examples :Mission do |testset, i|
 	before(:all) do
 		@ts = testset[:answers]
-		@ts.select{|k,v| "Hash" == v.class.name}
-			.values.each{|v| v.transform_keys!{|k| k.intern}}
 		@mission = testset[:mission]
 	end #before :all
 
 	context "#{testset[:name]} (##{i+1})" do
 		define "@num_by_tier" do
 			it "knows the number of rewards per rotation" do
-				expect(mission.num_by_tier).to eql(ts[:num_by_tier])
+				expect(@mission.num_by_tier).to eql(@ts[:num_by_tier])
 			end
 		end #define num_by_tier
 		define "@chance_tier" do
 			it "knows the chance of getting a number of rewards of a tier" do
-				expect(mission.chance_tier).to(
-					be_equal_array_hash_within(1e-7, ts[:chance_tier])
+				expect(@mission.chance_tier).to(
+					be_equal_array_hash_within(1e-7, @ts[:chance_tier])
 				)
 			end
 		end #define chance_tier
 		define "@chance_each" do
 			it "knows the average chance of getting a number of a certain reward of a tier" do
-				expect(mission.chance_each).to(
-					be_equal_array_hash_within(1e-7, ts[:chance_each])
+				expect(@mission.chance_each).to(
+					be_equal_array_hash_within(1e-7, @ts[:chance_each])
 				)
 			end
 		end #define chance_each
 		define "@mean_tier" do
 			it "knows the average number of rewards of each tier for one rotation/full runthrough" do
-				expect(mission.mean_tier).to(
-					be_equal_hash_within(1e-7, ts[:mean_tier])
+				expect(@mission.mean_tier).to(
+					be_equal_hash_within(1e-7, @ts[:mean_tier])
 				)
 			end
 		end #@mean_tier
 		define "@mean_each" do
 			it "knows the average number of a specific reward of a tier for one rotation or full runthrough" do
-				expect(mission.mean_each).to(
-					be_equal_hash_within(1e-7, ts[:mean_each])
+				expect(@mission.mean_each).to(
+					be_equal_hash_within(1e-7, @ts[:mean_each])
 				)
 			end
 		end #@mean_each
-		define "@tiers" do
-			it "knows which tiers of rewards it has" do
-				expect(mission.tiers).to eql(Set.new(ts[:tiers]))
+		# define "@tiers" do	# covered by @tier_rot.keys
+		# 	it "knows which tiers of rewards it has" do
+		# 		expect(@mission.tiers).to eql(Set.new(@ts[:tiers]))
+		# 	end
+		# end #@tiers
+		define "@tier_rot" do
+			it "knows which rotations each tier is present in" do
+				expect(@mission.tier_rot).to eql @ts[:tier_rot]
 			end
-		end #@tiers
+		end #@tier_rot"
 	end #context #{ts[:name]}
 end #shared_examples Mission
 
@@ -68,8 +71,11 @@ RSpec.shared_examples :MissionClass do |mission_class|
 			ts[:i] = i
 			ts[:answers].transform_keys!{|k| k.intern}
 				.values.each{|v| v.transform_keys!{|k| k.intern}}
-			ts[:answers][:tiers] = Set.new(ts[:tiers].map{|tier| tier.intern})
-			include_examples :Mission, ts
+			# ts[:answers][:tiers] = Set.new(ts[:tiers].map{|tier| tier.intern})
+			ts[:answers][:tier_rot].transform_values!{|v|
+				Set.new(v.map{|r| "nil" == r ? nil : r.intern})
+			}
+			include_examples :Mission, ts, i
 		} #testsets.each
 	end #RSpec.define "#{cname}"
 end #shared_examples MissionClass
