fb472ae2116b3073890fc40f8051479bf875642b
James Wescott <james@wescottdesign.com>
2018-10-17 15:01:55 -0700

    tidied up mission_spec

--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -3,10 +3,6 @@ require 'json'
 require 'set'
 require 'globals'
 
-# Put json in fixtures/mission.json
-# json = Hash.new
-# JSON.parse(File.read("spec/fixtures/endless.json"))
-
 TOL = 1e-7
 
 RSpec.shared_context :Mission do |mc, testsets|
@@ -17,17 +13,12 @@ RSpec.shared_context :Mission do |mc, testsets|
 		ts[:mission] = mc.new(ts[:data])
 		ts[:answers].transform_keys!{|k| k.intern}
 			.values.each{|v| v.transform_keys!{|k| k.intern}}
-		# ts[:answers][:tiers] = Set.new(ts[:tiers].map{|tier| tier.intern})
 		ts[:answers][:tier_rot].transform_values!{|v|
 			Set.new(v.map{|r| "nil" == r ? nil : r.intern})
 		}
-	# }
-			# define "@num_by_tier" do
 				it "##{i+1} (#{ts[:name]}) knows the number of rewards per rotation" do
 					expect(ts[:mission].num_by_tier).to eql(ts[:answers][:num_by_tier])
 				end
-			# end #define @num_by_tier
-			# define "@chance_tier" do
 				it "##{i+1} (#{ts[:name]}) knows the chance of getting a number of rewards of a tier" do
 					ans = ts[:answers][:chance_tier]
 					act = ts[:mission].chance_tier
@@ -35,39 +26,24 @@ RSpec.shared_context :Mission do |mc, testsets|
 						match(ts[:answers][:chance_tier].transform_values{|v| match(v.map{|c| a_value_within(TOL).of(c)})})
 					)
 				end
-			# end #define @chance_tier
-			# define "@chance_each" do
 				it "##{i+1} (#{ts[:name]}) knows the average chance of getting a number of a certain reward of a tier" do
 					expect(ts[:mission].chance_each).to(
 						match(ts[:answers][:chance_each].transform_values{|v| match(v.map{|c| a_value_within(TOL).of(c)})})
 					)
 				end
-			# end #define @chance_each
-			# define "@mean_tier" do
 				it "##{i+1} (#{ts[:name]}) knows the average number of rewards of each tier for one rotation/full runthrough" do
 					expect(ts[:mission].mean_tier).to(
 						match(ts[:answers][:mean_tier].transform_values{|v| a_value_within(TOL).of(v)})
 					)
 				end
-			# end #define @mean_tier
-			# define "@mean_each" do
 				it "##{i+1} (#{ts[:name]}) knows the average number of a specific reward of a tier for one rotation or full runthrough" do
 					expect(ts[:mission].mean_each).to(
 						match(ts[:answers][:mean_each].transform_values{|v| a_value_within(TOL).of(v)})
 					)
 				end
-			# end #define @mean_each
-			# define "@tiers" do	# covered by @tier_rot.keys
-			# 	it "##{i+1} (#{ts[:name]}) knows which tiers of rewards it has" do
-			# 		expect(ts[:mission].tiers).to eql(Set.new(ts[:answers][:tiers]))
-			# 	end
-			# end #@tiers
-			# define "@tier_rot" do
 				it "##{i+1} (#{ts[:name]}) knows which rotations each tier is present in" do
 					expect(ts[:mission].tier_rot).to eql ts[:answers][:tier_rot]
 				end
-			# end #define @tier_rot"
-		# end #context #{ts[:name]}
 	} #testsets.each
 end #shared_examples Mission
 
